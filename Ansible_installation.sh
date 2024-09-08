babf244656a401bdb102079025b56f876828c6ed
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-09-08 21:19:34 +0530

    Update Ansible_installation.sh
    list of extensions added

--- Ansible_installation.sh
+++ Ansible_installation.sh
@@ -2,4 +2,8 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -yansible --version
+sudo apt install ansible -y
+ansible --version
+#IN VS code download below extension by redhat
+#yaml
+#ansible
