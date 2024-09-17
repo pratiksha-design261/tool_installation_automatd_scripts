3ead8954bea2b59b990f3609c99c6d4e30260a65
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-09-17 16:05:11 +0530

    Update jenkins_intsallation.sh

--- jenkins_intsallation.sh
+++ jenkins_intsallation.sh
@@ -7,5 +7,7 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
+echo -e "\nJava Version :\n"
 java -version
+echo -e "\nJenkins Version :\n"
 jenkins --version
