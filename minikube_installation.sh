90d1521be3cda5651770b741846084751c5fd200
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-10-07 01:14:49 +0530

    Update minikube_installation.sh

--- minikube_installation.sh
+++ minikube_installation.sh
@@ -5,3 +5,18 @@ sudo apt install -y curl wget apt-transport-https
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube version
+
+#Install Kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version -o yaml
+
+#Start Minikube
+minikube start --driver=docker
+
+#Add Addns
+minikube start --addons=ingress --cpus=2 --cni=flannel --install-addons=true --kubernetes-version=stable --memory=6g
+
+# Check status
+minikube status
